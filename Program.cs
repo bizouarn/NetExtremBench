5f94d341f41b0d1760802856e06918149e532771
Aymeric BIZOUARN <a.bizouarn@herakles.com>
2025-07-16 15:10:21 +0200

    Delete SpanBench

--- Program.cs
+++ Program.cs
@@ -4,5 +4,3 @@ using BenchmarkDotNet.Running;
 
 Summary summary;
 summary = BenchmarkRunner.Run<ToStringCost>();
-//summary = BenchmarkRunner.Run<Pair>();
-//summary = BenchmarkRunner.Run<Multiply>();
